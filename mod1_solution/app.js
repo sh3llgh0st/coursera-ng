665cbf88b0f98060711851bd4381f05e6d8c794d
sh3llgh0st <ben.vanaken@gmail.com>
2017-02-19 13:02:03 +0100

    uploading the working version without extras

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,7 +1,40 @@
 /**
  *
  * @authors Ben Van Aken (ben.vanaken@gmail.com)
- * @date    2017-02-18 13:46:47
+ * @date    2017-02-19 12:24:25
  * @version $Id$
  */
 
+(function(){
+    'use strict';
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController ($scope) {
+        $scope.outputMsg = "";
+        $scope.inputMsg = "";
+
+        console.log($scope.inputMsg);
+        console.log($scope.outputMsg);
+
+        $scope.CheckLunch = function() {
+
+
+
+            if ($scope.inputMsg == "") {
+                $scope.outputMsg = "Please enter data first";
+            } else {
+                var size = $scope.inputMsg.split(',').length;
+                console.log("The size is: " + size);
+                if (size > 3) {
+                    $scope.outputMsg = "Too Much!";
+                } else {
+                    $scope.outputMsg = "Enjoy!";
+                };
+            };
+          };
+    };
+
+
+})();
